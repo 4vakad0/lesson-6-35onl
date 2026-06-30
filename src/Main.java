8597589515201c3b1d13c6006ed844ea06b12979
Anatoly Finaggev <finageev.99@mail.ru>
2026-06-30 21:44:06 +0300

    result v.1

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        CreditCard creditCard1 = new CreditCard(500, 111111111);
-        CreditCard creditCard2 = new CreditCard(500, 222222222);
-        CreditCard creditCard3 = new CreditCard(500, 333333333);
 
-        creditCard1.deposit(200);
-        creditCard2.withdraw(200);
-        creditCard3.withdraw(25);
-
-        creditCard1.getInfo();
-        creditCard2.getInfo();
-        creditCard3.getInfo();
 
 
 
